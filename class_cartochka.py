4830cdebdf9f984d7662156d3341fecd876700bf
ildar <ildarets@mail.ru>
2019-11-25 22:21:51 +0300

    Initial commit

---
+++ class_cartochka.py
@@ -0,0 +1,64 @@
+import random
+
+
+class Card:
+    """
+    класс, который генерирует карточки со случайными числами
+    """
+
+    def __init__(self):
+        self.number = None
+        self.card_list = []
+        self.i = 0
+        self.stroka = []
+        self.card_constuct = []
+        self.full_card = []
+        self.full_card_str = []
+        self.index_card = []
+        self.full_card_ready = []
+        for i in range(30):
+            self.full_card.append(0)
+
+    def card_full(self):
+
+        while len(self.card_list) != 15:
+            self.number = random.randint(1, 91)
+            if self.number in self.card_list:
+                continue
+            else:
+                self.card_list.append(self.number)
+
+        while len(self.index_card) != 15:
+            self.ind = random.randint(0, 30)
+            if self.ind in self.index_card:
+                continue
+            else:
+                self.index_card.append(self.ind)
+
+        for i in range(len(self.card_list)):
+            self.full_card[self.index_card[i]] = self.card_list[i]
+
+        for i in self.full_card:
+            if i == 0:
+                i = ' '
+            self.full_card_str.append(str(i))
+
+        for i in range(1, 4):
+            self.full_card_ready.append(self.full_card_str[0*i:5*i])
+
+        self.full_card_ready.append(self.full_card_str[0:10])
+        self.full_card_ready.append(self.full_card_str[10:20])
+        self.full_card_ready.append(self.full_card_str[20:30])
+
+        return self.card_list, self.index_card, self.full_card, len(self.card_list), len(self.index_card), len(
+            self.full_card), self.full_card_str,self.full_card_ready
+
+
+
+
+
+
+
+card1 = Card()
+print(card1.card_full())
+# print(card1.card_const())
